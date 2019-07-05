f3eb75b3ca447b9f74b453d0cfc9c247c9324efa
Julian Sagen <juliansagen@Julians-MacBook-Pro.local>
2019-07-05 13:46:52 +0200

    Solved file loading bug

--- src/util/parseFile.js
+++ src/util/parseFile.js
@@ -1,14 +1,17 @@
 import papaparse from 'papaparse';
 
 export default function ParseFile(file, setAttributes, setDataset, defaultAttributeType) {
-  papaparse.parse(file, {
-    complete(results) {
-      if (results.data.length > 0) {
-        const headers = results.data[0];
-        const attributeTypeModel = defaultAttributeType;
-        setAttributes(headers.map(field => ({ field, attributeTypeModel })));
-        setDataset(results.data);
-      }
-    },
-  });
+  console.log(file);
+  if (file) {
+    papaparse.parse(file, {
+      complete(results) {
+        if (results.data.length > 0) {
+          const headers = results.data[0];
+          const attributeTypeModel = defaultAttributeType;
+          setAttributes(headers.map(field => ({ field, attributeTypeModel })));
+          setDataset(results.data);
+        }
+      },
+    });
+  }
 }
