0b567f5a3fbf3fc4f6fc41b174d9ff42cc3e2e18
JeremiahUy <jery_3000@hotmail.com>
2019-07-08 11:18:48 +0200

    ADD height setting on XAxis

--- src/components/DistributionOfRiskGraph/RiskDistributionGraph.jsx
+++ src/components/DistributionOfRiskGraph/RiskDistributionGraph.jsx
@@ -25,7 +25,7 @@ const RiskDistributionGraph = (props) => {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis type="category" dataKey="name" angle={45} textAnchor="start" interval={0} />
+      <XAxis type="category" dataKey="name" angle={45} textAnchor="start" interval={0} height={80} />
       <YAxis type="number" ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} datakey="risk" />
       <Tooltip />
       <Legend />
