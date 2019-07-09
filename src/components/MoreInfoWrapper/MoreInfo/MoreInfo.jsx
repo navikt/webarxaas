45b9a5b8d42614ec0e8182de4e63e557c7e3f60f
vartdalen <viktor.v.johansen@hotmail.com>
2019-07-09 17:28:45 +0200

    REFACTOR MoreInfo component CREATE MoreInfoWrapper component

--- src/components/MoreInfoWrapper/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfoWrapper/MoreInfo/MoreInfo.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const MoreInfo = () => {
-  const content = (
-    <div>
-      <h1>To display Æ, Ø, and Å, make sure the dataset is saved as a CSV file with UTF-8</h1>
-    </div>
+const MoreInfo = (props) => {
+  const { showMoreInfo } = props;
 
-  );
+  let content = '';
+
+  if (showMoreInfo) {
+    content = (
+      <h1>To display Æ, Ø, and Å, make sure the dataset is saved as a CSV file with UTF-8</h1>
+    );
+  }
+
   return content;
 };
+
 export default MoreInfo;
