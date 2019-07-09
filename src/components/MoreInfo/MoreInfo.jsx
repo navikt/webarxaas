d413cccdce44f667d59b73dfd382691cc0a59563
andre groseth <andregroseth@hotmail.com>
2019-07-09 14:29:45 +0200

    ADD/helpText

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const MoreInfo = () => {
   const content = (
     <div>
-      <h1>Lorem </h1>
-      <h1>Ipsum</h1>
+      <h1>To display ÆØÅ make sure the dataset is saved as a CSV file with UTF-8</h1>
     </div>
 
   );
