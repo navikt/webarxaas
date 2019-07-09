357292e451d4294eecae39d6719a563164583c3c
andre groseth <andregroseth@hotmail.com>
2019-07-09 14:55:47 +0200

    refactorHelpText

--- src/components/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfo/MoreInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MoreInfo = () => {
   const content = (
     <div>
-      <h1>To display ÆØÅ make sure the dataset is saved as a CSV file with UTF-8</h1>
+      <h1>To display Æ, Ø, or Å make sure the dataset is saved as a CSV file with UTF-8</h1>
     </div>
 
   );
