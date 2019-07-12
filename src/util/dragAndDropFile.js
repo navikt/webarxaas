a12fd381e83c8582e8da34eebcd0afa680f30c6d
vartdalen <viktor.v.johansen@hotmail.com>
2019-07-12 13:22:56 +0200

    REFACTOR and optimization of js

--- src/util/dragAndDropFile.js
+++ src/util/dragAndDropFile.js
@@ -3,53 +3,42 @@ import ParseFile from './parseFile';
 export default function dragAndDropFile(
   setAttributes, setDataset, defaultAttributeType, setLoadingDataset,
 ) {
-  // feature detection for drag&drop upload
-  const isAdvancedUpload = (function () {
+  const form = document.querySelector('.import-file-dataset-form');
+  const input = form.querySelector('.import-file-dataset-input');
+
+  const isSupportedBrowser = () => {
     const div = document.createElement('div');
     return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-  }());
+  };
 
-  // applying the effect for every form
-  const forms = document.querySelectorAll('.import-file-dataset-form');
-  Array.prototype.forEach.call(forms, (form) => {
-    const input = form.querySelector('.import-file-dataset-input');
+  if (isSupportedBrowser) {
+    form.classList.add('has-advanced-upload');
 
-    // drag&drop files if the feature is available
-    if (isAdvancedUpload) {
-      form.classList.add('has-advanced-upload'); // letting CSS know drag&drop is supported by the browser
+    form.addEventListener('drop', (e) => {
+      input.files = e.dataTransfer.files;
+      setLoadingDataset(true);
+      ParseFile(
+        input.files[0], setAttributes, setDataset, defaultAttributeType, setLoadingDataset,
+      );
+    });
 
-      form.addEventListener('drop', (e) => {
-        if (isAdvancedUpload) {
-          input.files = e.dataTransfer.files;
-          setLoadingDataset(true);
-          ParseFile(
-            input.files[0], setAttributes, setDataset, defaultAttributeType, setLoadingDataset,
-          );
-        }
+    // css / animations
+    ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach((event) => {
+      form.addEventListener(event, (e) => {
+        // preventing unwanted behaviours
+        e.preventDefault();
+        e.stopPropagation();
       });
-
-      // css
-      ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach((event) => {
-        form.addEventListener(event, (e) => {
-          // preventing unwanted behaviours
-          e.preventDefault();
-          e.stopPropagation();
-        });
+    });
+    ['dragover', 'dragenter'].forEach((event) => {
+      form.addEventListener(event, () => {
+        form.classList.add('is-dragover');
       });
-      ['dragover', 'dragenter'].forEach((event) => {
-        form.addEventListener(event, () => {
-          form.classList.add('is-dragover');
-        });
+    });
+    ['dragleave', 'dragend', 'drop'].forEach((event) => {
+      form.addEventListener(event, () => {
+        form.classList.remove('is-dragover');
       });
-      ['dragleave', 'dragend', 'drop'].forEach((event) => {
-        form.addEventListener(event, () => {
-          form.classList.remove('is-dragover');
-        });
-      });
-    }
-
-    // Firefox focus bug fix for file input
-    input.addEventListener('focus', () => { input.classList.add('has-focus'); });
-    input.addEventListener('blur', () => { input.classList.remove('has-focus'); });
-  });
+    });
+  }
 }
