8ce595d0976ab7f776dc87339926fe9643241938
vartdalen <viktor.v.johansen@hotmail.com>
2019-07-16 15:22:47 +0200

    REMOVE unnecessary and imprecislely named analysis-result-wrapper (css class)

--- src/components/ResultWrapper/AnalysisWrapper/AnalysisWrapper.jsx
+++ src/components/ResultWrapper/AnalysisWrapper/AnalysisWrapper.jsx
@@ -21,39 +21,36 @@ const AnalysisWrapper = (props) => {
     content = (
       <div className="analysis-wrapper">
         <h4>Result</h4>
-        <div className="analysis-result-wrapper">
-          <div className="risk-barometer">
-            <RiskChart risk={
-              parseFloat(response.reIdentificationRisk.measures.estimated_prosecutor_risk) * 100}
-            />
-          </div>
-          <Container>
-            <Row>
-              <Col sm={12}>
-                <Ekspanderbartpanel tittel="Risk Metrics" border>
-                  <ReIdentificationRiskTable reIdentificationRisk={response.reIdentificationRisk} />
-                </Ekspanderbartpanel>
-              </Col>
-              <Col sm={12}>
-                <Ekspanderbartpanel tittel="Distribution Of Risk" border>
-                  <div className="risk-distribution-graph">
-                    <RiskDistributionGraph RiskDistribution={response.distributionOfRisk
+        <div className="risk-barometer">
+          <RiskChart risk={
+            parseFloat(response.reIdentificationRisk.measures.estimated_prosecutor_risk) * 100}
+          />
+        </div>
+        <Container>
+          <Row>
+            <Col sm={12}>
+              <Ekspanderbartpanel tittel="Risk Metrics" border>
+                <ReIdentificationRiskTable reIdentificationRisk={response.reIdentificationRisk} />
+              </Ekspanderbartpanel>
+            </Col>
+            <Col sm={12}>
+              <Ekspanderbartpanel tittel="Distribution Of Risk" border>
+                <div className="risk-distribution-graph">
+                  <RiskDistributionGraph RiskDistribution={response.distributionOfRisk
+                    .riskIntervalList}
+                  />
+                </div>
+                <div className="risk-distribution-metrics">
+                  <Ekspanderbartpanel tittel="More Information" border>
+                    <DistributionOfRiskTable riskIntervalList={response.distributionOfRisk
                       .riskIntervalList}
                     />
-                  </div>
-                  <div className="risk-distribution-metrics">
-                    <Ekspanderbartpanel tittel="More Information" border>
-                      <DistributionOfRiskTable riskIntervalList={response.distributionOfRisk
-                        .riskIntervalList}
-                      />
-                    </Ekspanderbartpanel>
-                  </div>
-                </Ekspanderbartpanel>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <br />
+                  </Ekspanderbartpanel>
+                </div>
+              </Ekspanderbartpanel>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
