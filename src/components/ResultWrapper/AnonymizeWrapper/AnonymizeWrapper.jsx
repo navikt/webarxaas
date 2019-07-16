0c358fdfe14981820c3e2d8d6b25c42106629a77
JeremiahUy <jery_3000@hotmail.com>
2019-07-16 13:17:36 +0200

    ADD suppression limit to be rendered on the page

--- src/components/ResultWrapper/AnonymizeWrapper/AnonymizeWrapper.jsx
+++ src/components/ResultWrapper/AnonymizeWrapper/AnonymizeWrapper.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import HierarchyImport from './HierachyImport/HeirarchyImport';
 import PrivacyModelManager from './PrivacyModel/PrivacyModelManager';
 import PrivacyModelTable from './PrivacyModel/PrivacyModelTable';
+import SuppressionLimit from './SuppressionLimit/SuppressionLimit';
 
 const AnonymizeWrapper = (props) => {
   const {
     attributes, setAttributes,
     privacyModels, setPrivacyModels,
+    suppressionLimit, setSuppressionLimit,
   } = props;
 
   const content = (
@@ -21,6 +23,12 @@ const AnonymizeWrapper = (props) => {
         <div className="col-4">
           <PrivacyModelManager privacyModels={privacyModels} setPrivacyModels={setPrivacyModels} />
           <PrivacyModelTable privacyModels={privacyModels} setPrivacyModels={setPrivacyModels} />
+        </div>
+        <div className="col-4">
+          <SuppressionLimit
+            suppressionLimit={suppressionLimit}
+            setSuppressionLimit={setSuppressionLimit}
+          />
         </div>
       </div>
     </div>
