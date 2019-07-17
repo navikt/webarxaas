241269f6db08efd98a7a412cdbfac28bae28d7fd
JeremiahUy <jery_3000@hotmail.com>
2019-07-17 15:12:30 +0200

    REFACTOR handlerequest bug that sends two request when anonymizing

--- src/util/handleRequest.js
+++ src/util/handleRequest.js
@@ -5,12 +5,11 @@ export default function handleRequest(
   setLoading, service, dataset, attributes,
   privacyModels = [], suppressionLimit = null, endpoint, setResponse,
 ) {
-  if (dataset && attributes) {
-    const payload = BuildPayload(dataset, attributes);
-    ArxRequest(setLoading, endpoint, payload, service, setResponse);
-  }
   if (dataset && attributes && privacyModels) {
     const payload = BuildPayload(dataset, attributes, privacyModels, suppressionLimit);
+    ArxRequest(setLoading, endpoint, payload, service, setResponse);
+  } else if (dataset && attributes) {
+    const payload = BuildPayload(dataset, attributes);
     ArxRequest(setLoading, endpoint, payload, service, setResponse);
   }
 }
