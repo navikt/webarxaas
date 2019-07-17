37aa9a94f8bcb55b42845aff07417bd447caef46
andre groseth <andregroseth@hotmail.com>
2019-07-17 15:29:30 +0200

    ADD attribute text information to more info

--- src/components/MoreInfoWrapper/MoreInfo/MoreInfo.jsx
+++ src/components/MoreInfoWrapper/MoreInfo/MoreInfo.jsx
@@ -14,26 +14,37 @@ const MoreInfo = (props) => {
 
         <div className="more-info-attribute">
           <p><b>CSV import file:</b></p>
-          <p>
-          To display Æ, Ø, and Å, make sure the imported dataset is saved as a CSV file with UTF-8
-          </p>
+          <ul>
+            <li>
+              <p>
+                To display Æ, Ø, and Å, make sure the imported dataset is saved as a CSV file with UTF-8
+              </p>
+            </li>
+          </ul>
           <p><b>Attribute types:</b></p>
-          <li>
-            <b>Identifying </b>
-               attributes will be removed from the dataset.
-          </li>
-          <li>
-            <b>Quasi-identifying </b>
-            attributes will be transformed.
-          </li>
-          <li>
-            <b>Sensitive </b>
-          attributes will be kept as-is but they can be protected using privacy models, such as t-closeness or l-diversity.
-          </li>
-          <li>
-            <b>Insensitive </b>
-            attributes will be kept unmodified.
-          </li>
+          <ul>
+            <li>
+              <b>Identifying </b>
+              attributes are associated with a high risk of re-identification. They will be removed from the dataset.
+              <br />
+              Typical examples are names or Social Security Numbers.
+            </li>
+            <li>
+              <b>Quasi-identifying </b>
+              attributes can in combination be used for re-identification attacks. They will be transformed. 
+              Typical examples are gender, date of birth and ZIP codes.
+            </li>
+            <li>
+              <b>Sensitive </b>
+              attributes encode properties with which individuals are not willing to be linked with. 
+              As such, they might be of interest to an attacker and, if disclosed, could cause harm to data subjects.
+              They will be kept unmodified but may be subject to further constraints, such as t-closeness or l-diversity. Typical examples are diagnoses.
+            </li>
+            <li>
+              <b>Insensitive </b>
+              attributes are not associated with privacy risks. They will be kept unmodified.
+            </li>
+          </ul>
         </div>
       </div>
     );
