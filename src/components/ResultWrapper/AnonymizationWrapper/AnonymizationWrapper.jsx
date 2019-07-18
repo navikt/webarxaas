c94619577aa28393ddd58c107c31bbe4032557e4
JeremiahUy <jery_3000@hotmail.com>
2019-07-18 09:24:12 +0200

    ADD loading spinner

--- src/components/ResultWrapper/AnonymizationWrapper/AnonymizationWrapper.jsx
+++ src/components/ResultWrapper/AnonymizationWrapper/AnonymizationWrapper.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import NavFrontendSpinner from 'nav-frontend-spinner';
 
 const AnonymizationWrapper = (props) => {
   const { response, loadingAnonymize } = props;
+  let content = ' ';
+  if (loadingAnonymize) {
+    content = (
+      <div className="anonymization-wrapper">
+        <NavFrontendSpinner transparent />
+      </div>
+    );
+  }
+  return content;
 };
 export default AnonymizationWrapper;
