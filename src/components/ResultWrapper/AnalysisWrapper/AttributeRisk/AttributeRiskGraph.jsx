76c7a9d5d70ffa79de40b5c0fc503de970b70520
JeremiahUy <jery_3000@hotmail.com>
2019-07-23 12:42:30 +0200

    REFACTOR to only render the unique columns of the dataset

--- src/components/ResultWrapper/AnalysisWrapper/AttributeRisk/AttributeRiskGraph.jsx
+++ src/components/ResultWrapper/AnalysisWrapper/AttributeRisk/AttributeRiskGraph.jsx
@@ -6,15 +6,13 @@ import './__css__/AttributeRiskGraph.css';
 
 const AttributeRiskGraph = (props) => {
   const { attributeRisk } = props;
-  const riskData = [];
-
-  attributeRisk.quasiIdentifierRiskList.forEach((obj) => {
-    riskData.push({
-      name: obj.identifier,
+  const riskData = attributeRisk.quasiIdentifierRiskList
+    .filter(quasiIdentifierRisk => quasiIdentifierRisk.identifier.length === 1)
+    .map(obj => ({
+      identifier: obj.identifier[0],
       distinction: (parseFloat(obj.distinction) * 100).toFixed(2),
       separation: (parseFloat(obj.separation) * 100).toFixed(2),
-    });
-  });
+    }));
 
   const content = (
     <div className="attribute-risk-graph">
@@ -27,11 +25,12 @@ const AttributeRiskGraph = (props) => {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis type="category" dataKey="name" angle={45} textAnchor="start" interval={0} height={80} />
+        <XAxis type="category" dataKey="identifier" angle={45} textAnchor="start" interval={0} height={80} />
         <YAxis type="number" ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} datakey="distinction" />
         <Tooltip />
         <Legend />
-        <Bar dataKey="distinction" name="Percentage" fill="#f54542" />
+        <Bar dataKey="distinction" name="Distinction" fill="#f54542" />
+        <Bar dataKey="separation" name="Separation" fill="#0f54f5" />
       </BarChart>
     </div>
   );
