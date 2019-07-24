9bd201ec1bfc2f5041e7c1025be31033cb0423b5
Julian Sagen <juliansagen@Julians-MacBook-Pro.local>
2019-07-24 16:34:41 +0200

    Test for checking if content is shown when moreInfoButton is pressed

--- src/components/NavbarMain/MoreInfoButton/__test__/MoreInfoButton.test.jsx
+++ src/components/NavbarMain/MoreInfoButton/__test__/MoreInfoButton.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from '../../../../enzyme';
+import { shallow, mount } from '../../../../enzyme';
 import MoreInfoButton from '../MoreInfoButton';
+import App from '../../../App/App';
 
 describe('test for MoreInfo button', () => {
   let wrapper;
@@ -9,5 +10,12 @@ describe('test for MoreInfo button', () => {
   });
   it('Render componet', () => {
     expect(wrapper.find('ToggleKnapp').length).toEqual(1);
+  });
+
+  it('Test button state change', () => {
+    const app = mount(<App />);
+    expect(app.contains(<h1>Information</h1>)).toEqual(false);
+    app.find('ToggleKnapp').simulate('click');
+    expect(app.contains(<h1>Information</h1>)).toEqual(true);
   });
 });
