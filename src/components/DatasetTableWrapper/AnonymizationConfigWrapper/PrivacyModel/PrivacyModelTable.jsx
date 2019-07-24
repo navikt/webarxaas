d9c2cec18f83a6b6403e04979dfb469bdded4793
Julian Sagen <juliansagen@Julians-MacBook-Pro.local>
2019-07-24 09:38:31 +0200

    Improved the formating of the the parameters in the table

--- src/components/DatasetTableWrapper/AnonymizationConfigWrapper/PrivacyModel/PrivacyModelTable.jsx
+++ src/components/DatasetTableWrapper/AnonymizationConfigWrapper/PrivacyModel/PrivacyModelTable.jsx
@@ -3,6 +3,7 @@ import handlePrivacyRemove from '../../../../util/handlePrivacyRemove';
 
 const PrivacyModelTable = (props) => {
   const { privacyModels, setPrivacyModels } = props;
+  console.log(privacyModels);
 
   const content = (
     <div className="privacy-model-table">
@@ -16,9 +17,20 @@ const PrivacyModelTable = (props) => {
         </thead>
         <tbody>
           {privacyModels.map((model, index) => (
-            <tr key={model}>
+            <tr key={model.privacyModel}>
               <td>{model.privacyModel}</td>
-              <td>{JSON.stringify(model.params)}</td>
+              <td>
+                {Object.entries(model.params).map(paramPair => (
+                  <div key={paramPair[0]}>
+                    <p>
+                      {paramPair[0]}
+                      {': '}
+                      {paramPair[1]}
+                    </p>
+                  </div>
+                ))}
+
+              </td>
               <td>
                 <button type="button" className="btn btn-danger btn-sm" onClick={() => handlePrivacyRemove(index, privacyModels, setPrivacyModels)}>Remove</button>
               </td>
