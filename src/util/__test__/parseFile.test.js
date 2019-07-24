57c3d4fbe5eed09f2960bb95426f813a2c2efc9a
JeremiahUy <jery_3000@hotmail.com>
2019-07-24 14:08:19 +0200

    REFACTOR to make file more readable

--- src/util/__test__/parseFile.test.js
+++ src/util/__test__/parseFile.test.js
@@ -4,31 +4,36 @@ import testCSVFile from './TestData/testCSVFile.csv';
 describe('Unit test for parsing csv files', () => {
   let testFile;
   let attributes = [];
+  let dataset = '';
+  let defaultAttributeType;
+  let loadingDataset = true;
+
   const setAttributes = (newAttributes) => {
     attributes = newAttributes;
   };
-  let dataset = '';
   const setDataset = (newDataset) => {
     dataset = newDataset;
   };
-  let defaultAttributeType;
-  let loadingDataset = true;
   const setLoadingDataset = (newLoadingDataset) => {
     loadingDataset = newLoadingDataset;
   };
+
   beforeAll(() => {
     const blob = new Blob([testCSVFile]);
     testFile = blob;
     defaultAttributeType = 'QUASIIDENTIFYING';
   });
+
   it('file correctly parse', () => {
     ParseFile(testFile, setAttributes, setDataset, defaultAttributeType, setLoadingDataset);
     expect(dataset.length).toEqual(1);
   });
+
   it('attributes correctly set', () => {
     ParseFile(testFile, setAttributes, setDataset, defaultAttributeType, setLoadingDataset);
     expect(attributes.length).toEqual(1);
   });
+
   it('loadingDataset to be set to false', () => {
     ParseFile(testFile, setAttributes, setDataset, defaultAttributeType, setLoadingDataset);
     expect(loadingDataset).toEqual(false);
