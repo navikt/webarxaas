1cd7e4a7c8b54074cb0089475c857422d7572496
vartdalen <viktor.v.johansen@hotmail.com>
2019-07-30 09:20:59 +0200

    REMOVE malfunctioning test

--- src/components/ResultWrapper/AnalysisWrapper/ReIdentificationRisk/ReIdentificationRiskTable/__tests__/ReIdentificationRiskTable.test.jsx
+++ src/components/ResultWrapper/AnalysisWrapper/ReIdentificationRisk/ReIdentificationRiskTable/__tests__/ReIdentificationRiskTable.test.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { shallow } from '../../../../../../enzyme';
 
-
 import ReIdentificationRiskTable from '../ReIdentificationRiskTable';
 import reIdentificationRiskResponse from './reIdentificationRiskResponse';
 
@@ -28,35 +27,6 @@ describe('reidentificationrisk table tests', () => {
       <td>5.00 %</td>,
       <td>0.10 %</td>,
       <td>0.00 %</td>];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const testCase of testCases) {
-      expect(wrapper.contains(testCase)).toBeTruthy();
-    }
-  });
-  it('table renders every label and value', () => {
-    const reIdentificationRisk = reIdentificationRiskResponse;
-    const wrapper = shallow(
-      <ReIdentificationRiskTable reIdentificationRisk={reIdentificationRisk} />,
-    );
-    const testCases = [
-      <tr><td>Lowest risk: </td><td>0.66 %</td></tr>,
-      <tr><td>Records affected by lowest risk: </td><td>40.12 %</td></tr>,
-      <tr><td>Average prosecutor risk: </td><td>5.00 %</td></tr>,
-      <tr><td>Highest prosecutor risk: </td><td>100.00 %</td></tr>,
-      <tr><td>Records affected by highest prosecutor risk: </td><td>0.10 %</td></tr>,
-      <tr><td>Prosecutor attacker success rate: </td><td>5.00 %</td></tr>,
-      <tr><td>Highest journalist risk: </td><td>100.00 %</td></tr>,
-      <tr><td>Records affected by highest journalist risk: </td><td>0.10 %</td></tr>,
-      <tr><td>Journalist attacker success rate: </td><td>5.00 %</td></tr>,
-      <tr><td>Marketer attacker success rate: </td><td>5.00 %</td></tr>,
-      <tr><td>Estimated prosecutor risk: </td><td>100.00 %</td></tr>,
-      <tr><td>Estimated journalist risk: </td><td>100.00 %</td></tr>,
-      <tr><td>Estimated marketer risk: </td><td>5.00 %</td></tr>,
-      <tr><td><b>Sample uniques: </b></td><td>0.10 %</td></tr>,
-      <tr><td>Population uniques: </td><td>0.00 %</td></tr>,
-      <tr><td>Population model: </td><td>ZAYATZ</td></tr>,
-      <tr><td><b>Quasi-identifiers: </b></td><td>Alder, Innvandrerbakgrunn, </td></tr>,
-    ];
     // eslint-disable-next-line no-restricted-syntax
     for (const testCase of testCases) {
       expect(wrapper.contains(testCase)).toBeTruthy();
