3c2382cb1d274ed9381e6fad87ceb57757e79814
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-07-31 16:28:34 +0200

    ADD previewing of anonymized data

--- src/components/AnonymizedDatasetWrapper/AnonymizedDatasetTable/AnonymizedDatasetTable.jsx
+++ src/components/AnonymizedDatasetWrapper/AnonymizedDatasetTable/AnonymizedDatasetTable.jsx
@@ -1,5 +1,6 @@
 import ReactTable from 'react-table';
 import React from 'react';
+import Alert from 'react-bootstrap/Alert';
 import 'react-table/react-table.css';
 
 const AnonymizedDatasetTable = (props) => {
@@ -9,10 +10,18 @@ const AnonymizedDatasetTable = (props) => {
     accessor: key,
   }));
 
-  const data = anonymizeResult.data.slice(1);
+  const dataset = anonymizeResult.data;
+  const data = dataset.slice(1, 101);
 
   const content = (
     <div className="anonymized-dataset-table">
+      <Alert variant="info">
+        Previewing first 100 of
+        {' '}
+        {dataset.length}
+        {' '}
+        rows in dataset
+      </Alert>
       <ReactTable
         data={data}
         columns={columns}
