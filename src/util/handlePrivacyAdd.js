9eb6c268b7871275c4e4ea1a3e006445eec682c2
JeremiahUy <jery_3000@hotmail.com>
2019-07-31 13:16:33 +0200

    ADD function to find index of k anonymity and replace value if found

--- src/util/handlePrivacyAdd.js
+++ src/util/handlePrivacyAdd.js
@@ -1,3 +1,10 @@
 export default function handlePrivacyAdd(model, privacyModels, setPrivacyModels) {
-  setPrivacyModels([...privacyModels, model]);
+  const index = privacyModels.findIndex(item => item.privacyModel === 'KANONYMITY');
+  if (model.privacyModel === 'KANONYMITY' && index !== -1) {
+    const newPrivacyModels = privacyModels;
+    newPrivacyModels[index] = model;
+    setPrivacyModels([...newPrivacyModels]);
+  } else {
+    setPrivacyModels([...privacyModels, model]);
+  }
 }
