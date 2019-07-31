cffab614fae21bee5cd2653cfbe15bec7e8c601a
JeremiahUy <jery_3000@hotmail.com>
2019-07-31 13:20:27 +0200

    ADD test for the new lines of code for handlePrivacyAdd

--- src/util/__test__/handlePrivacyAdd.test.js
+++ src/util/__test__/handlePrivacyAdd.test.js
@@ -20,4 +20,13 @@ describe('test for handlePrivacyAdd', () => {
     handlePrivacyAdd(model, privacyModels, setPrivacyModels);
     expect(privacyModels).toEqual(expected);
   });
+
+  it('adding k-anon should overwrite existing k-anon', () => {
+    const expected = [{ privacyModel: 'KANONYMITY', params: { k: '10' } },
+      { privacyModel: 'TCLOSENESS_EQUAL_DISTANCE', params: { t: '1', field: 'Alder' } },
+      { privacyModel: 'LDIVERSITY_DISTINCT', params: { l: '2', field: 'Navn' } }];
+    const newModel = { privacyModel: 'KANONYMITY', params: { k: '10' } };
+    handlePrivacyAdd(newModel, privacyModels, setPrivacyModels);
+    expect(privacyModels).toEqual(expected);
+  });
 });
