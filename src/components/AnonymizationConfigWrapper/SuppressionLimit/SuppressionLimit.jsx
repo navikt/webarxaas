cdb36331089f153c206497334e41286151a2a5a5
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-08-01 17:33:09 +0200

    CREATE new suppression limit input

--- src/components/AnonymizationConfigWrapper/SuppressionLimit/SuppressionLimit.jsx
+++ src/components/AnonymizationConfigWrapper/SuppressionLimit/SuppressionLimit.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Chip from '@material-ui/core/Chip';
+
+
+const useStyles = makeStyles({
+  textField: {
+    width: 200,
+  },
+
+  chip: {
+    margin: '2em 0em 2em 0',
+  },
+});
+
 
 const SuppressionLimit = (props) => {
+  const classes = useStyles();
   const { suppressionLimit, setSuppressionLimit } = props;
+  const suppressionLabel = `Suppression Limit: ${suppressionLimit} %`;
+
+  const updateSuppressionLimit = (newLimit) => {
+    const limit = parseInt(newLimit, 10) || 0;
+    if (limit > 100) {
+      setSuppressionLimit(100);
+    } else if (limit < 0) {
+      setSuppressionLimit(0);
+    } else {
+      setSuppressionLimit(limit);
+    }
+  };
 
   const content = (
     <div className="suppression-limit" align="center">
-      <label htmlFor="limit">
-                Limit:
-        <input type="number" id="limit" min="0" max="1" step="0.001" />
-      </label>
-      <button type="button" className="btn btn-outline-primary" onClick={() => setSuppressionLimit(document.getElementById('limit').value)}>Add Suppression Limit</button>
-      Suppression Limit:
-      {suppressionLimit}
-      <button type="button" className="btn btn-danger btn-sm" onClick={() => setSuppressionLimit(null)}>Remove</button>
+      <TextField
+        id="standard-with-placeholder"
+        label="Set Suppression Limit"
+        type="number"
+        defaultValue={0}
+        onChange={event => updateSuppressionLimit(event.target.value)}
+        InputProps={{
+          inputProps: { min: 0, max: 100 },
+          endAdornment: <InputAdornment position="end">%</InputAdornment>,
+        }}
+        className={classes.textField}
+        margin="normal"
+      />
+      <br />
+      <Chip
+        label={suppressionLabel}
+        clickable
+        className={classes.chip}
+        color="primary"
+      />
     </div>
   );
   return content;
