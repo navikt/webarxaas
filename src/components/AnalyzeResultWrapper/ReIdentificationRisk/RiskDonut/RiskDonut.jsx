f6185534102087324a8ec1c9bdd4483a813681b4
JeremiahUy <jery_3000@hotmail.com>
2019-08-01 16:05:23 +0200

    ADD color fill for chart

--- src/components/AnalyzeResultWrapper/ReIdentificationRisk/RiskDonut/RiskDonut.jsx
+++ src/components/AnalyzeResultWrapper/ReIdentificationRisk/RiskDonut/RiskDonut.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Chart from 'react-apexcharts';
 
@@ -5,19 +6,34 @@ const RiskDonut = (props) => {
   const { highestRisk, affectedRecords } = props;
 
   const optionsRadial = {
-    labels: ['Percent'],
     plotOptions: {
       radialBar: {
+        startAngle: -90,
+        endAngle: 270,
         hollow: {
           size: '70%',
         },
+        dataLabels: {
+          total: {
+            show: true,
+            label: 'Highest prosecutor risk',
+            formatter(w) {
+              return `${highestRisk}%`;
+            },
+          },
+        },
       },
     },
+    fill: {
+      colors: ['#f54542', '#f54542'],
+    },
+    labels: ['Records affected by highest prosecutor risk', 'Highest prosecutor risk'],
   };
 
   const content = (
     <div className="risk-donut">
-      <Chart options={optionsRadial} series={[affectedRecords, highestRisk]} type="radialBar" width="280" />
+      <p>{`Records affected by highest prosecutor risk: ${affectedRecords}%`}</p>
+      <Chart options={optionsRadial} series={[affectedRecords, highestRisk]} type="radialBar" width="300" align="center" />
     </div>
   );
   return content;
