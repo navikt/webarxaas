277b59e2b8b7d4f15ca53b86f57d9240846f96a9
vartdalen <viktor.v.johansen@hotmail.com>
2019-08-02 16:06:52 +0200

    FIX ImportDataset css

--- src/components/ImportDatasetWrapper/ImportDataset/ImportDataset.jsx
+++ src/components/ImportDatasetWrapper/ImportDataset/ImportDataset.jsx
@@ -21,9 +21,8 @@ const ImportDataset = (props) => {
   const content = (
     <div className="import-dataset">
       <form method="" action="" encType="multipart/form-data" noValidate="" className={importDatasetFormStyle}>
-        <div className="container mt-1 align-self-center">
+        <div className="container container-fluid mt-1">
           <div className={onRow}>
-            <div className="col-2" />
             <div className="col">
               <svg className="import-dataset-icon" width="45" height="40" viewBox="0 0 50 43"><path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z" /></svg>
             </div>
@@ -49,7 +48,6 @@ const ImportDataset = (props) => {
                 }
               />
             </div>
-            <div className="col-4" />
           </div>
         </div>
       </form>
