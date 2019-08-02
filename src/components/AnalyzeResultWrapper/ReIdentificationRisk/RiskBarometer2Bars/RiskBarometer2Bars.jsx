e541b0791bf13bc423ff15b06df7ea0d4d43c8b8
JeremiahUy <jery_3000@hotmail.com>
2019-08-02 10:41:31 +0200

    ADD new component to render 2 values in a barometer

---
+++ src/components/AnalyzeResultWrapper/ReIdentificationRisk/RiskBarometer2Bars/RiskBarometer2Bars.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+const RiskBarometer2Bars = (props) => {
+  const { highestRisk, affectedRecords } = props;
+  const riskData = [{
+    name: '', highestRisk, affectedRecords,
+  }];
+
+  const content = (
+    <div className="risk-chart">
+      <BarChart
+        width={1000}
+        height={150}
+        layout="vertical"
+        data={riskData}
+        margin={{
+          top: 20, right: 40, left: 50, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" horizontalPoints={[20]} />
+        <XAxis type="number" ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} />
+        <YAxis type="category" dataKey="name" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="highestRisk" fill="#f54542" barSize={40} name="Highest Prosecutor Risk" />
+        <Bar dataKey="affectedRecords" fill="#f54542" barSize={40} name="Records Affected by Highest Prosecutor Risk" />
+      </BarChart>
+    </div>
+  );
+  return content;
+};
+
+export default RiskBarometer2Bars;
+RiskBarometer2Bars.defaultProps = { risk: 100 };
