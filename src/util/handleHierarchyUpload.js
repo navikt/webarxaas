8cc6988920330a0bfe499813b31933d928f0331f
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-08-05 15:54:41 +0200

    FIX rerender of state by returning new object from function

--- src/util/handleHierarchyUpload.js
+++ src/util/handleHierarchyUpload.js
@@ -3,7 +3,7 @@ import papaparse from 'papaparse';
 export default function handleHierarchyUpload(file, index, attributes, setAttributes) {
   papaparse.parse(file, {
     complete(hierarchy) {
-      const newAttributes = attributes;
+      const newAttributes = [...attributes];
       newAttributes[index] = {
         ...newAttributes[index],
         hierarchy: hierarchy.data,
