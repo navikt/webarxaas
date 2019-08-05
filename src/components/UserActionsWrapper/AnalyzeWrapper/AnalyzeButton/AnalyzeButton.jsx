a2aa93b6f347ac4f06e6b5a35c00709ee3b87aeb
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-08-05 17:33:39 +0200

    FIX styling so button doesnt overflow on small screens

--- src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeButton/AnalyzeButton.jsx
+++ src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeButton/AnalyzeButton.jsx
@@ -8,7 +8,7 @@ const AnalyzeButton = (props) => {
   } = props;
 
   const content = (
-    <div className="analyze-button col" style={{ margin: '5em 0 5em 0' }}>
+    <div style={{ margin: '5em 0 5em 0' }}>
       <Knapp
         htmlType="button"
         onClick={
