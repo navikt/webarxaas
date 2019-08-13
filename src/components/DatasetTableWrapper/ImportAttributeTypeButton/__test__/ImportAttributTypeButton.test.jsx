1569cd6b4e45833b1344bb8eca800db486961433
JeremiahUy <jery_3000@hotmail.com>
2019-08-13 12:40:14 +0200

    ADD test for rendering of import export attribute type button

---
+++ src/components/DatasetTableWrapper/ImportAttributeTypeButton/__test__/ImportAttributTypeButton.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { shallow } from '../../../../enzyme';
+import ImportAttributeTypeButton from '../ImportAttributeTypeButton';
+
+describe('ImportAttributeTypeButton: Render test', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    wrapper = shallow(
+      <ImportAttributeTypeButton />,
+    );
+  });
+
+  it('Render test for button', () => {
+    expect(wrapper.find(Button).length).toEqual(1);
+  });
+});
