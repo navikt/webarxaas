9c801505865d605834835d9cdfcae5182f1e475e
andre groseth <andregroseth@hotmail.com>
2019-08-16 14:22:09 +0200

    FEATURE add helpText marketerModel

--- src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/ReIdentificationRisk/ReIdentificationRiskTable/ReIdentificationRiskTable.jsx
+++ src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/ReIdentificationRisk/ReIdentificationRiskTable/ReIdentificationRiskTable.jsx
@@ -23,6 +23,16 @@ const dialogContentJournalistModel = (
   </p>
 );
 
+const dialogTitleMarketerModel = 'Marketer model:';
+
+const dialogContentMarketerModel = (
+  <p>
+    In the marketer model the attacker does not target a specific individual but aims at
+     re-identifying a high number of individuals. An attack can therefore only be considered
+      successful if a larger fraction of the records could be re-identified.
+  </p>
+);
+
 const ReIdentificationRiskTable = (props) => {
   const { reIdentificationRisk } = props;
   const content = (
@@ -126,6 +136,10 @@ const ReIdentificationRiskTable = (props) => {
 
             <tr>
               <td><p><b>Marketer model: </b></p></td>
+              <HelpText
+                dialogTitle={dialogTitleMarketerModel}
+                dialogContent={dialogContentMarketerModel}
+              />
             </tr>
             <tr>
               <td>Estimated marketer risk: </td>
