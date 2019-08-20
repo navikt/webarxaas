311ce037e59b59333b71decfa96708faa37ce4e2
andre groseth <andregroseth@hotmail.com>
2019-08-20 09:49:07 +0200

    FEATRURE add privacyModels help text

--- src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizationConfigWrapper/AnonymizationConfigWrapper.jsx
+++ src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizationConfigWrapper/AnonymizationConfigWrapper.jsx
@@ -6,6 +6,7 @@ import AnonymizeButton from './AnonymizeButton/AnonymizeButton';
 import PrivacyModelWrapper from './PrivacyModelWrapper/PrivacyModelWrapper';
 import './__css__/AnonymizationConfigWrapper.css';
 import HierarchiesHelpText from './AnonymizationConfigWrapperHelpText/HierarchiesHelpText';
+import PrivacyModelHelpText from './AnonymizationConfigWrapperHelpText/PrivacyModelHelpText';
 import HelpText from '../../../HelpTextWrapper/HelpText';
 
 const AnonymizationConfigWrapper = (props) => {
@@ -16,50 +17,6 @@ const AnonymizationConfigWrapper = (props) => {
     setLoadingAnonymize, dataset, setResponse,
     endpoint,
   } = props;
-
-  const dialogTitlePrivacyModel = 'Privacy Models:';
-
-  const dialogContentPrivacyModel = (
-    <div>
-      <p><b>k-Anonymity</b></p>
-      <ul>
-        <li>
-          <p>
-            This well-known privacy model aims at protecting dataset from
-             re-identification in the prosecutor model.
-            <br />
-            A dataset is k-anonymous if each record cannot be distinguished
-             from at least k-1 other records regarding the quasi-identifiers.
-            <br />
-            Each group of indistinguishable records forms a so-called equivalence class.
-          </p>
-        </li>
-      </ul>
-      <p><b>ℓ-Diversity</b></p>
-      <ul>
-        <li>
-          <p>
-            This privacy model can be used to protect data against attribute
-             disclosure by ensuring that each sensitive attribute has at least
-            <br />
-            ℓ well represented values in each equivalence class.
-          </p>
-        </li>
-      </ul>
-      <p><b>t-Closeness</b></p>
-      <ul>
-        <li>
-          <p>
-            This privacy model can also be used to protect data from attribute
-             disclosure. It requires that the distributions of values of a
-            <br />
-            sensitive attribute within each equivalence class must have a distance
-             of not more than t to the distribution of the attribute values in the input dataset.
-          </p>
-        </li>
-      </ul>
-    </div>
-  );
 
   const dialogTitleSuppressionLimit = 'Suppression Limit:';
 
@@ -88,10 +45,7 @@ const AnonymizationConfigWrapper = (props) => {
           <div className="privacy-model-manager-card col">
             <Card>
               <CardHeader title="Set Privacy Model(s)" />
-              <HelpText
-                dialogTitle={dialogTitlePrivacyModel}
-                dialogContent={dialogContentPrivacyModel}
-              />
+              <PrivacyModelHelpText />
               <PrivacyModelWrapper
                 attributes={attributes}
                 privacyModels={privacyModels}
---
+++ src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizationConfigWrapper/AnonymizationConfigWrapperHelpText/PrivacyModelHelpText.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import HelpText from '../../../../HelpTextWrapper/HelpText';
+
+const PrivacyModelHelpText = () => {
+  const dialogTitlePrivacyModel = 'Privacy Models:';
+
+  const dialogContentPrivacyModel = (
+    <div>
+      <p><b>k-Anonymity</b></p>
+      <ul>
+        <li>
+          <p>
+            This well-known privacy model aims at protecting dataset from
+             re-identification in the prosecutor model.
+            <br />
+            A dataset is k-anonymous if each record cannot be distinguished
+             from at least k-1 other records regarding the quasi-identifiers.
+            <br />
+            Each group of indistinguishable records forms a so-called equivalence class.
+          </p>
+        </li>
+      </ul>
+      <p><b>ℓ-Diversity</b></p>
+      <ul>
+        <li>
+          <p>
+            This privacy model can be used to protect data against attribute
+             disclosure by ensuring that each sensitive attribute has at least
+            <br />
+            ℓ well represented values in each equivalence class.
+          </p>
+        </li>
+      </ul>
+      <p><b>t-Closeness</b></p>
+      <ul>
+        <li>
+          <p>
+            This privacy model can also be used to protect data from attribute
+             disclosure. It requires that the distributions of values of a
+            <br />
+            sensitive attribute within each equivalence class must have a distance
+             of not more than t to the distribution of the attribute values in the input dataset.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+  const content = (
+    <HelpText
+      dialogTitle={dialogTitlePrivacyModel}
+      dialogContent={dialogContentPrivacyModel}
+    />
+  );
+  return content;
+};
+
+export default PrivacyModelHelpText;
