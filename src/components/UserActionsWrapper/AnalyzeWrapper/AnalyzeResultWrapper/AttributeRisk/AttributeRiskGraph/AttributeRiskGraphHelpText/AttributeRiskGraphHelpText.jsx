d394e2486dc98433d816e98bbb7318fbb931d304
andre groseth <andregroseth@hotmail.com>
2019-08-22 14:49:19 +0200

    FEATURE add help text attribute risk graph

---
+++ src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/AttributeRisk/AttributeRiskGraph/AttributeRiskGraphHelpText/AttributeRiskGraphHelpText.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import HelpText from '../../../../../../HelpTextWrapper/HelpText';
+
+const AttributeRiskGraphHelpText = () => {
+  const dialogTitleDistinctionAndSeparation = 'Distinction and Separation:';
+
+  const dialogContentDistinctionAndSeparation = (
+    <div>
+      <p>
+        <b>Distinction</b>
+        {' '}
+        represents the ratio between the unique values
+        for the quasi-identifiers and the total number of records.
+        <br />
+        <br />
+        <b>Separation</b>
+        {' '}
+        represents the ratio between pairs of records with at least one
+         different value for their quasi-identifiers, and the total number
+          of ways that two different records can be paired. In general, a
+           higher distinction and separation are indicators that the
+            quasi-identifiers are more likely to re-identify a record.
+      </p>
+    </div>
+  );
+  const content = (
+    <HelpText
+      dialogTitle={dialogTitleDistinctionAndSeparation}
+      dialogContent={dialogContentDistinctionAndSeparation}
+    />
+  );
+  return content;
+};
+
+export default AttributeRiskGraphHelpText;
