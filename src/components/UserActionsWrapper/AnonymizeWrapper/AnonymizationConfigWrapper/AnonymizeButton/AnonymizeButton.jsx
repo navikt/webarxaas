0fce99dd1c4990c7adce71c790b67a85d79b6fba
andre groseth <andregroseth@hotmail.com>
2019-08-26 13:53:38 +0200

    FEATURE add handle hierarchy file upload

--- src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizationConfigWrapper/AnonymizeButton/AnonymizeButton.jsx
+++ src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizationConfigWrapper/AnonymizeButton/AnonymizeButton.jsx
@@ -7,14 +7,15 @@ const AnonymizeButton = (props) => {
   const {
     setLoadingAnonymize, datasetFile,
     attributes, privacyModels, suppressionLimit,
-    endpoint, setResponse,
+    endpoint, setResponse, hierarchies,
   } = props;
 
   const content = (
     <div className="anonymize-button small">
       <Knapp onClick={() => {
         setLoadingAnonymize(true);
-        handleRequest(setLoadingAnonymize, 'anonymize/file', endpoint, setResponse, datasetFile, attributes, privacyModels, suppressionLimit);
+        handleRequest(setLoadingAnonymize, 'anonymize/file', endpoint, setResponse, datasetFile, attributes,
+          privacyModels, suppressionLimit, hierarchies);
       }}
       >
         Anonymize Dataset
