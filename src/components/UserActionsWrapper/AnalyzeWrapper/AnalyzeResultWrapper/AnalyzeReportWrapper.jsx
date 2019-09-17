1911e9d22e9b7db6ba4b890776d6afbaa3f6d3d9
JeremiahUy <jery_3000@hotmail.com>
2019-09-17 13:20:54 +0200

    ADD attributes used to be part of the report

--- src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/AnalyzeReportWrapper.jsx
+++ src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/AnalyzeReportWrapper.jsx
@@ -5,23 +5,70 @@ import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 
 const AnalyzeReportWrapper = (props) => {
-  const { response, fileName } = props;
+  const { response, fileName, attributes } = props;
 
   pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
   const reportFileName = fileName.toString().replace('.csv', '').concat('_report.pdf');
+  const now = new Date();
+  const date = ((now.getDate() < 10 ? '0' : '') + now.getDate());
+  const month = ((now.getMonth() < 10 ? '0' : '') + now.getMonth());
+  const year = now.getFullYear();
+  const hour = ((now.getHours() < 10 ? '0' : '') + now.getHours());
+  const minute = ((now.getMinutes() < 10 ? '0' : '') + now.getMinutes());
+
+  const attributesUsed = [['Column name', 'Attribute Type']];
+  attributes.forEach((item) => {
+    attributesUsed.push([item.field, item.attributeTypeModel]);
+  });
+
   const reportContent = {
     content: [
-      'Test test test',
-      'hope this works',
       {
-        text: 'This is a header',
+        text: 'Re-Identification Analysis Report',
         style: 'header',
+        alignment: 'center',
       },
+      '\n',
+      {
+        text: [
+          { text: 'Analysis Report for: ', bold: true },
+          `${fileName.toString()}`,
+        ],
+      },
+      {
+        text: [
+          { text: 'Report Time: ', bold: true },
+          `${hour}:${minute}`,
+        ],
+      },
+      {
+        text: [
+          { text: 'Report Date: ', bold: true },
+          `${date}.${month}.${year}`,
+        ],
+      },
+      '\n',
+      {
+        text: 'Attributes Used',
+        style: 'subheader',
+      },
+      'The table shows which attributes was used on each column, when analyzing for re-identification risk.',
+      {
+        table: {
+          widths: ['*', '*'],
+          body: attributesUsed,
+        },
+      },
+
     ],
     styles: {
       header: {
         fontSize: 18,
+        bold: true,
+      },
+      subheader: {
+        fontSize: 15,
         bold: true,
       },
     },
