a52da3924887c69377d983efe80ef3d4f220e9b4
andre groseth <andregroseth@hotmail.com>
2019-09-19 16:03:21 +0200

    ADD AnalyzeManual text

---
+++ src/components/UserManualPage/AnalyzeManual/AnalyzeManual.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AnalyzeManual = () => {
+  const content = (
+    <div className="analyze-manual">
+      <h1>Analyze re-identification risk manual</h1>
+      <br />
+      <p>
+        Info: Clicking the question mark in the web application
+         will display the corresponding help text.
+      </p>
+    </div>
+  );
+  return content;
+};
+
+export default AnalyzeManual;
