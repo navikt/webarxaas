7ff7061ec202b14caf07f94e9cb5161efc750d20
andre groseth <andregroseth@hotmail.com>
2019-09-26 15:41:21 +0200

    Refactor moved text to general information.

--- src/components/UserManualPage/AttributeTypesDescription/AttributeTypesDescription.jsx
+++ src/components/UserManualPage/AttributeTypesDescription/AttributeTypesDescription.jsx
@@ -40,56 +40,6 @@ const AttributeTypesDescription = () => {
       </ul>
       <br />
       <p>Attribute types are by default set to quasi-identifying.</p>
-      <p><b>Prosecutor model:</b></p>
-      <ul>
-        <li>
-          <p>
-            In the prosecutor model the attacker targets a specific individual,
-            <br />
-            and it is assumed that the attacker already knows that data about the
-             individual, is contained in the dataset.
-          </p>
-        </li>
-      </ul>
-      <p><b>Privacy models:</b></p>
-      <br />
-      <p><b>k-Anonymity</b></p>
-      <ul>
-        <li>
-          <p>
-            This well-known privacy model aims at protecting dataset from
-             re-identification in the prosecutor model.
-            <br />
-            A dataset is k-anonymous if each record cannot be distinguished from at
-             least k-1 other records regarding the quasi-identifiers.
-            <br />
-            Each group of indistinguishable records forms a so-called equivalence class.
-          </p>
-        </li>
-      </ul>
-      <p><b>ℓ-Diversity</b></p>
-      <ul>
-        <li>
-          <p>
-            This privacy model can be used to protect data against attribute disclosure
-             by ensuring that each sensitive attribute has at least
-            <br />
-            ℓ well represented values in each equivalence class.
-          </p>
-        </li>
-      </ul>
-      <p><b>t-Closeness</b></p>
-      <ul>
-        <li>
-          <p>
-            This privacy model can also be used to protect data from attribute disclosure.
-             It requires that the distributions of values of a
-            <br />
-            sensitive attribute within each equivalence class must have a distance of not
-             more than t to the distribution of the attribute values in the input dataset.
-          </p>
-        </li>
-      </ul>
     </div>
   );
 
