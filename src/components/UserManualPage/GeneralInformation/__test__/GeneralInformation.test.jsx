44f1a80ac8918b103c3bd183506b831de3de7792
andre groseth <andregroseth@hotmail.com>
2019-09-26 09:49:07 +0200

    Refactor Test

--- src/components/UserManualPage/GeneralInformation/__test__/GeneralInformation.test.jsx
+++ src/components/UserManualPage/GeneralInformation/__test__/GeneralInformation.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GeneralInformation from '../GeneralInformation';
 import { shallow } from '../../../../enzyme';
 import QuestionMarkHelpText from '../GeneralInformationHelpText/QuestionMarkHelpText';
+import AttributeTypesDescription from '../../AttributeTypesDescription/AttributeTypesDescription';
 
 describe('GeneralInformation: Render test', () => {
   let wrapper;
@@ -14,8 +15,7 @@ describe('GeneralInformation: Render test', () => {
     expect(wrapper.contains(<h1>General Information</h1>)).toBeTruthy();
     expect(wrapper.contains('Question mark help text:')).toBeTruthy();
     expect(wrapper.contains('CSV import file:')).toBeTruthy();
-    expect(wrapper.contains('Attribute types:')).toBeTruthy();
-    expect(wrapper.contains('Privacy models:')).toBeTruthy();
+    expect(wrapper.find(AttributeTypesDescription).length).toEqual(1);
     expect(wrapper.find(QuestionMarkHelpText).length).toEqual(1);
   });
 });
