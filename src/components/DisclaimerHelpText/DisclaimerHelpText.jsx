a56cfe08388cfbf0a58589df5ee8ce423be6ee4d
andre groseth <andregroseth@hotmail.com>
2019-10-02 15:45:20 +0200

    ADD Dislaimer content text

--- src/components/DisclaimerHelpText/DisclaimerHelpText.jsx
+++ src/components/DisclaimerHelpText/DisclaimerHelpText.jsx
@@ -8,8 +8,30 @@ const DisclaimerHelpText = () => {
   const dialogContentHelpText = (
     <div>
       <p>
-        content coming soon.
+        The results of the analysis and anonymization is
+        calculated by using the “ARX - Data Anonymization Tool” and is not a guarantee
+        that the dataset is anonymous, legally or otherwise. The results show the different
+        re-identification risks connected to the dataset and is anonymised based on the
+        specifications that the user applied.
       </p>
+      <ul>
+        <li>
+            The default parameters for the privacy models provided by the applications do not
+            guarantee that the dataset is sufficiently anonymised.
+        </li>
+        <li>
+            The user is advised that all privacy models have limitations and may not cover
+            all possible present or future risk scenarios.
+        </li>
+        <li>
+            No data or informasjon is stored after anonymization and analyzation is
+            completed, except for logs for operational purposes. The logs contain ip requests
+            origin, size of the request and privacy models used.
+        </li>
+        <li>
+            The user is responsible for determining the acceptable risk level.
+        </li>
+      </ul>
     </div>
   );
 
