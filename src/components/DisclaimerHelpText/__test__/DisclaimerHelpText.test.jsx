42a2aaba6579076be8c7ec122433b58dc5a3e003
andre groseth <andregroseth@hotmail.com>
2019-10-02 15:55:14 +0200

    ADD test for disclaimerhelptext component

---
+++ src/components/DisclaimerHelpText/__test__/DisclaimerHelpText.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from '../../../enzyme';
+import DisclaimerHelpText from '../DisclaimerHelpText';
+import HelpText from '../../HelpTextWrapper/HelpText';
+
+describe('Test for disclaimer help text component', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    wrapper = shallow(<DisclaimerHelpText />);
+  });
+
+  it('Test for component rendering', () => {
+    expect(wrapper.contains(
+      <p className="disclaimer-text">
+      By using this service you acknowledge
+       that you have read and understand the
+      </p>,
+    )).toBeTruthy();
+
+    expect(wrapper.find(HelpText).length).toEqual(1);
+  });
+});
