810f278050c1fa4ebce30e0e92b593d2b715ba41
JeremiahUy <jery_3000@hotmail.com>
2019-10-03 14:02:39 +0200

    ADD if clause to catch different error and give different error message

--- src/util/arxRequest.js
+++ src/util/arxRequest.js
@@ -14,6 +14,11 @@ export default function ArxRequest(setLoadingFunction, endpoint, formData, servi
     setLoadingFunction(false);
   }).catch((error) => {
     const errorMessage = error;
+    if (error instanceof SyntaxError) {
+      errorMessage.message = `${error.message}. A common cause of this error is the dataset size being too large, for the web browser to handle.`;
+    } else if (error instanceof TypeError) {
+      errorMessage.message = `${error.message}. No service connection.`;
+    }
     errorMessage.message = `${error}`;
     setResponse(errorMessage);
     setLoadingFunction(false);
