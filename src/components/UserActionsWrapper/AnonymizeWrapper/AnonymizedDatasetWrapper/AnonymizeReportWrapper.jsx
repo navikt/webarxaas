b8d4c87db32e804a902479b97f6f76c940cdee6d
JeremiahUy <jery_3000@hotmail.com>
2019-10-04 14:06:30 +0200

    ADD feature for exporting anonymization report

--- src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizedDatasetWrapper/AnonymizeReportWrapper.jsx
+++ src/components/UserActionsWrapper/AnonymizeWrapper/AnonymizedDatasetWrapper/AnonymizeReportWrapper.jsx
@@ -4,6 +4,8 @@ import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import generateAnalysisReport from '../../../../util/generateAnalysisReport';
 import generateAnonymizationReport from '../../../../util/generateAnonymizationReport';
+import handleJsonExport from '../../../../util/handleJsonExport';
+import './__css__/AnonymizedDatasetWrapper.css';
 
 const AnonymizeReportWrapper = (props) => {
   const {
@@ -19,22 +21,42 @@ const AnonymizeReportWrapper = (props) => {
   };
 
   const content = (
-    <div className="download-report-button small" style={{ margin: '5em 0 5em 0' }}>
-      <Knapp
-        htmlType="button"
-        onClick={
-          () => {
-            const reportFileName = fileName.toString().replace('.csv', '').concat('_report.pdf');
-            const analysisReportContent = generateAnalysisReport(response.riskProfile,
-              attributes, document);
-            const reportContent = generateAnonymizationReport(response,
-              analysisReportContent, privacyModels, suppressionLimit);
-            pdfMake.createPdf(reportContent).download(reportFileName);
+    <div>
+      <div className="download-report-button small">
+        <Knapp
+          htmlType="button"
+          onClick={
+            () => {
+              const reportFileName = fileName.toString().replace('.csv', '').concat('_report.pdf');
+              const analysisReportContent = generateAnalysisReport(response.riskProfile,
+                attributes, document);
+              const reportContent = generateAnonymizationReport(response,
+                analysisReportContent, privacyModels, suppressionLimit);
+              pdfMake.createPdf(reportContent).download(reportFileName);
+            }
           }
-        }
-      >
-        Download Anonymization Report as PDF
-      </Knapp>
+        >
+          Download Anonymization Report as PDF
+        </Knapp>
+      </div>
+      <div className="download-report-button small">
+        <Knapp
+          htmlType="button"
+          onClick={
+            () => {
+              const objectToExport = {
+                privacyModels,
+                suppressionLimit,
+                attributeGeneralization: response.anonymizeResult.metrics.attributeGeneralization,
+                riskProfile: response.riskProfile,
+              };
+              handleJsonExport(objectToExport, fileName, '_report.json', 'anonymization_report.json');
+            }
+          }
+        >
+          Download Anonymization Report as JSON
+        </Knapp>
+      </div>
     </div>
   );
 
