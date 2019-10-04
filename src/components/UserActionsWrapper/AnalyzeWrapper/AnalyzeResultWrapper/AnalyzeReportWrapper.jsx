355ac7e3e12f2522d467190a28200ff5b141d23c
JeremiahUy <jery_3000@hotmail.com>
2019-10-04 13:42:37 +0200

    ADD css for download report buttons

--- src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/AnalyzeReportWrapper.jsx
+++ src/components/UserActionsWrapper/AnalyzeWrapper/AnalyzeResultWrapper/AnalyzeReportWrapper.jsx
@@ -4,6 +4,7 @@ import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import generateAnalysisReport from '../../../../util/generateAnalysisReport';
 import handleJsonExport from '../../../../util/handleJsonExport';
+import './__css__/AnalysisWrapper.css';
 
 const AnalyzeReportWrapper = (props) => {
   const { response, fileName, attributes } = props;
@@ -19,29 +20,33 @@ const AnalyzeReportWrapper = (props) => {
 
   if (response.reIdentificationRisk) {
     content = (
-      <div className="download-report-button small" style={{ margin: '5em 0 5em 0' }}>
-        <Knapp
-          htmlType="button"
-          onClick={
-            () => {
-              const reportFileName = fileName.toString().replace('.csv', '').concat('_report.pdf');
-              const reportContent = generateAnalysisReport(response, attributes, document);
-              pdfMake.createPdf(reportContent).download(reportFileName);
+      <div>
+        <div className="download-report-button small">
+          <Knapp
+            htmlType="button"
+            onClick={
+              () => {
+                const reportFileName = fileName.toString().replace('.csv', '').concat('_report.pdf');
+                const reportContent = generateAnalysisReport(response, attributes, document);
+                pdfMake.createPdf(reportContent).download(reportFileName);
+              }
             }
-          }
-        >
-          Download Analysis Report as PDF
-        </Knapp>
-        <Knapp
-          htmlType="button"
-          onClick={
-            () => {
-              handleJsonExport(response, fileName, '_analysis_report.json', 'analysis_report.json');
+          >
+            Download Analysis Report as PDF
+          </Knapp>
+        </div>
+        <div className="download-report-button small">
+          <Knapp
+            htmlType="button"
+            onClick={
+              () => {
+                handleJsonExport(response, fileName, '_analysis_report.json', 'analysis_report.json');
+              }
             }
-          }
-        >
-          Download Analysis Report as JSON
-        </Knapp>
+          >
+            Download Analysis Report as JSON
+          </Knapp>
+        </div>
       </div>
     );
   }
