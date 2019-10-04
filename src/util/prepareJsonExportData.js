11d14c05a2ea22319a31a9b144c4d18e32dbd539
JeremiahUy <jery_3000@hotmail.com>
2019-10-04 15:13:31 +0200

    REFACTOR to only include prosecutor risk in the json report

--- src/util/prepareJsonExportData.js
+++ src/util/prepareJsonExportData.js
@@ -10,11 +10,39 @@ export default function prepareJsonExportData(riskProfile, attributes,
     );
   });
 
+  const newRiskProfile = {
+    reIdentificationRisk: {
+      measures: {
+        records_affected_by_highest_prosecutor_risk: riskProfile.reIdentificationRisk
+          .measures.records_affected_by_highest_prosecutor_risk,
+        sample_uniques: riskProfile.reIdentificationRisk.measures.sample_uniques,
+        lowest_risk: riskProfile.reIdentificationRisk.measures.lowest_risk,
+        estimated_prosecutor_risk: riskProfile.reIdentificationRisk
+          .measures.estimated_prosecutor_risk,
+        records_affected_by_lowest_risk: riskProfile.reIdentificationRisk
+          .measures.records_affected_by_lowest_risk,
+        average_prosecutor_risk: riskProfile.reIdentificationRisk
+          .measures.average_prosecutor_risk,
+        highest_prosecutor_risk: riskProfile.reIdentificationRisk
+          .measures.highest_prosecutor_risk,
+      },
+      attackerSuccessRate: {
+        successRates: {
+          Prosecutor_attacker_success_rate: riskProfile.reIdentificationRisk
+            .attackerSuccessRate.successRates.Prosecutor_attacker_success_rate,
+        },
+      },
+      quasiIdentifiers: riskProfile.reIdentificationRisk.quasiIdentifiers,
+    },
+    distributionOfRisk: riskProfile.distributionOfRisk,
+    attributeRisk: riskProfile.attributeRisk,
+  };
+
   return {
     attributeTypeUsed: newAttributes,
     privacyModelsUsed: privacyModels,
     suppressionLimitUsed: suppressionLimit,
     attributeGeneralization,
-    riskProfile,
+    riskProfile: newRiskProfile,
   };
 }
